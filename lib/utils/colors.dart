f94d86429c93d65a50fe079c9deb7aa4ab07f282
yunustahayilmaz <yunustahayilmaz@gmail.com>
2024-04-02 22:19:26 +0300

    colors added.

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class MobimColors {
+  static const primary = Color(0xff404040);
+  static const secondary = Color.fromARGB(255, 123, 122, 122);
+  static const tertiary = Color(0xffee6568);
+  static const success = Color.fromARGB(255, 8, 186, 88);
+  static const warning = Color.fromARGB(255, 249, 146, 44);
+  static const error = Color.fromARGB(255, 249, 69, 69);
+}
