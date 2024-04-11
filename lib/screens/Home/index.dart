8015b953f9e34a891e93465722d0fecd44303036
Yunus Taha Yılmaz <48367134+yunustaha@users.noreply.github.com>
2024-04-11 13:21:21 +0300

    Drawer added.

--- lib/screens/Home/index.dart
+++ lib/screens/Home/index.dart
@@ -1,24 +1,30 @@
-import 'dart:developer';
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:giri/screens/Home/pages/Customer/AddCustomer/index.dart';
+import 'package:giri/screens/Home/pages/Customer/index.dart';
 import 'package:giri/screens/Home/pages/Main/index.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:giri/utils/index.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatelessWidget with GiriMixin {
   HomeScreen({super.key});
+
+  Size size = PlatformDispatcher.instance.views.first.physicalSize / PlatformDispatcher.instance.views.first.devicePixelRatio;
+
+  final GlobalKey<ScaffoldState> _globalkey = GlobalKey<ScaffoldState>();
 
   List<Widget> _buildScreens() {
     return [
       const MainPage(),
       const MainPage(),
       Container(),
-      const MainPage(),
+      CustomerPage(),
       const MainPage(),
     ];
   }
 
-  List<PersistentBottomNavBarItem> _navBarsItems() {
+  List<PersistentBottomNavBarItem> _navBarsItems(BuildContext context) {
     return [
       PersistentBottomNavBarItem(
         icon: const Icon(CupertinoIcons.home),
@@ -40,47 +46,8 @@ class HomeScreen extends StatelessWidget {
         title: ("Müşteri Ekle"),
         activeColorPrimary: CupertinoColors.activeBlue,
         inactiveColorPrimary: CupertinoColors.systemGrey,
-        onPressed: (context) {
-          if (context != null) {
-            showMaterialModalBottomSheet(
-              context: context,
-              builder: (context) => SingleChildScrollView(
-                controller: ModalScrollController.of(context),
-                child: Container(
-                  height: 500,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    child: Stack(
-                      children: [
-                        Align(
-                          alignment: Alignment.center,
-                          child: Container(
-                            decoration: BoxDecoration(color: Color.fromARGB(255, 123, 122, 122), borderRadius: BorderRadius.all(Radius.circular(10))),
-                            width: 40,
-                            height: 7,
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.pop(context);
-                              FocusScope.of(context).unfocus();
-                            },
-                            child: Icon(
-                              Icons.close,
-                              color: Color.fromARGB(255, 123, 122, 122),
-                              size: 30,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            );
-          }
+        onPressed: (_) {
+          openBottomSheet(context, AddCustomerForm());
         },
       ),
       PersistentBottomNavBarItem(
@@ -102,11 +69,44 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      key: _globalkey,
+      appBar: _buildAppBar(context),
+      drawer: _buildDrawer(context),
+      body: _buildBody(context),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(45),
+      child: AppBar(
+        title: const Text(
+          'Giri',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 24),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          iconSize: 30,
+          icon: const Icon(CupertinoIcons.list_bullet),
+          color: Colors.white,
+          tooltip: 'Menu',
+          onPressed: () {
+            _globalkey.currentState?.openDrawer();
+          },
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
     return PersistentTabView(
       context,
       controller: _controller,
       screens: _buildScreens(),
-      items: _navBarsItems(),
+      items: _navBarsItems(context),
       decoration: NavBarDecoration(
         borderRadius: BorderRadius.circular(10.0),
         colorBehindNavBar: Colors.white,
@@ -123,7 +123,43 @@ class HomeScreen extends StatelessWidget {
       //   curve: Curves.ease,
       //   duration: Duration(milliseconds: 200),
       // ),
-      navBarStyle: NavBarStyle.style16, // Choose the nav bar style with this property.
+      navBarStyle: NavBarStyle.style16,
+    );
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const UserAccountsDrawerHeader(
+            currentAccountPicture: FlutterLogo(),
+            accountName: Text("Flutter"),
+            accountEmail: Text("https://flutter.dev"),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+            ),
+          ),
+          ListTile(
+            title: Text('Mesajlar'),
+            subtitle: Text("Mesajlarınızı okuyun"),
+            leading: Icon(Icons.question_answer),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Ayarlar'),
+            subtitle: Text("Uygulamayı kişiselleştirin"),
+            leading: Icon(Icons.settings),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/Home/pages/Main/index.dart
+++ lib/screens/Home/pages/Main/index.dart
@@ -1,38 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(45),
-        child: AppBar(
-          title: const Text(
-            'Giri',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 24),
-          ),
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.exit_to_app),
-              color: Colors.white,
-              tooltip: 'Setting Icon',
-              onPressed: () {
-                final box = Hive.box('auth');
-                box.put('token', null);
-
-                Navigator.of(context, rootNavigator: true).pushReplacementNamed('/signIn');
-              },
-            ),
-          ],
-          backgroundColor: Theme.of(context).colorScheme.primary,
-        ),
-      ),
-      body: const Text('Home'),
-    );
+    return const Text('Home');
   }
 }
